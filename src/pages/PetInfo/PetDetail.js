37a7fd7ce2e0ef44172a2b272a7f5b98b6860138
tiga1207@naver.com <tiga1207@naver.com>
2023-07-26 21:57:01 +0900

    (petinfo의 list & detail page에 삭제기능 추가)

--- src/pages/PetInfo/PetDetail.js
+++ src/pages/PetInfo/PetDetail.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState,useCallback } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import BottomTabNavigation from "../../components/Navigation/NavigationBottom.js";
 import BoardHeader from "../../components/Header/HeaderBoard.js";
 // import PetDummy from "../../dummy/PetDummy.js";
 import useApiHooks from "../../api/BaseApi";
+import LayoutUserExist from "../../components/Layout/LayoutUserExist.js";
 
 
 function PetDetail() {
   const nav = useNavigate();
   const { getApi } = useApiHooks();
+  const { deleteApi } = useApiHooks();
   const [petContent, setPetContent] = useState({
     image: null,
     petName: "",
@@ -18,7 +20,6 @@ function PetDetail() {
     petBreed: "",
     petGender: "",
     petWeight: "",
-    disease: "",
     petIntroduction: "",
   });
   const [searchParams, setSearchParams] = useSearchParams();
@@ -37,6 +38,18 @@ function PetDetail() {
     });
   }, []);
 
+  const onDel = useCallback(() => {
+    const petId = searchParams.get("detailID");
+    deleteApi({ url: `/api/pet/${petId}`}).then((resp) => {
+      console.log(resp);
+      if (resp.status === 200) {
+        // nav(-1)-> 만약 이전에 다른 행위(작성, 등등)을 했을 경우 오류가 발생할 가능성이 있음. 
+        //그렇다고 (nav`/petinfo' 를 하면 detail페이지는 문제 없으나, 리스트에 있는 삭제버튼을 누를 시 
+        // 새로고침이 안되어 사용자가 새로고침을 누르지 않는 이상 계속 해당 게시물이 없어지지 않는 것 처럼 보임)
+      }
+    });
+  }, []);
+
   // const PetDummys = PetDummy.PetDummy.find(
   //   (PetDummy) => PetDummy.id === parseInt(searchparams.get("detailID"))
   // );
@@ -46,6 +59,7 @@ function PetDetail() {
   // }
 
   return (
+    <LayoutUserExist>
     <div id="main">
       <div id="counsel_header">
         <BoardHeader title="내 반려동물 상세정보 페이지" />
@@ -58,7 +72,6 @@ function PetDetail() {
           <div>펫 성별: {petContent.petGender}</div>
           <div>펫 품종: {petContent.petBreed}</div>
           <div>펫 체중: {petContent.petWeight}kg</div>
-          <div>펫 질병: {petContent.disease}</div>
           <div>펫 소개: {petContent.petIntroduction}</div>
           {/* <div>작성일: {petContent.createdtime}</div> */}
           <div
@@ -72,15 +85,16 @@ function PetDetail() {
               // }
             }}
           >
-            <span>펫 정보 수정하기</span>
+            수정하기
           </div>
-          <div>
-            <span>삭제</span>
-          </div>
+          <button className="btn_delete" onClick={onDel}>
+            삭제
+          </button>
         </div>
       </div>
       <BottomTabNavigation />
     </div>
+    </LayoutUserExist>
   );
 }
 
--- src/pages/PetInfo/PetList.js
+++ src/pages/PetInfo/PetList.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useRecoilValue } from "recoil";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import BtnFloat from "../../components/Button/BtnFloat.js";
 import BottomTabNavigation from "../../components/Navigation/NavigationBottom.js";
 import BoardHeader from "../../components/Header/HeaderBoard.js";
@@ -8,7 +8,6 @@ import { UserState } from "../../state/User";
 import defaultImg from "../../resources/image/userDefault.png";
 import LayoutUserExist from "../../components/Layout/LayoutUserExist.js";
 import useApiHooks from "../../api/BaseApi.js";
-// import PetDummy from "../../dummy/PetDummy.js";
 import ImgWrapper from "../../components/Wrapper/ImgWrapper.js";
 import { isEmpty } from "../../components/Utils/Utils.js";
 
@@ -17,60 +16,102 @@ function PetList() {
   const ctime = new Date();
   const nav = useNavigate();
   const { getApi } = useApiHooks();
-  const [petList,setPetList] = useState([]);
+  const { deleteApi } = useApiHooks();
+  const [petList, setPetList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  
 
   useEffect(() => {
-    getApi({ url: `/api/pet`}).then(resp=>{
-      if(resp.status ===200){
-        console.log(resp.data.data)
+    getApi({ url: `/api/pet` }).then((resp) => {
+      if (resp.status === 200) {
+        console.log(resp.data.data);
         setPetList(resp.data.data);
       }
     });
   }, []);
 
+  // const onDel = useCallback(() => {
+  //   deleteApi({ url: `/api/pet/${petId}` }).then((resp) => {
+  //     console.log(resp);
+  //     if (resp.status === 200) {
+  //       nav(-1);
+  //     }
+  //   });
+  // }, []);
+
   return (
-    
+    // 삭제 버튼도 추가하여 리스트창에서도 삭제 가능하도록 구현해보기.
+    <LayoutUserExist>
       <div id="main">
         <BoardHeader title="펫 리스트" />
         <div id="counsel-board" className="content flex-column">
           <div className="content scroll-hide board-list">
             <div className="list-col">
-              {!isEmpty(petList)>0&&petList.map((p) => {
-                console.log(p)
-                return <div
-                  key={p.petId}
-                  className="list-item"
-                  onClick={() => {
-                    nav(`/petinfo/detail?detailID=${p.petId}`);
-                  }}
-                >
-                  <div className="list-title">
-                    <ImgWrapper 
-                    src={process.env.REACT_APP_API_URL  + p.petImage} 
-                    alt={"내 펫 이미지"} width="30px" height="30px" borderRadius="50%" defaultImg={defaultImg} />
-                  </div>
-                  <div>{p.petName}</div>
-                  <div style={{ display: "flex", width: "100%" }}>
-                    <div className="list-reg-user"></div>
-                    <div className="list-reg-dt">
-                      작성일: {ctime.toISOString()}
+              {!isEmpty(petList) > 0 &&
+                petList.map((p) => {
+                  console.log(p);
+                  return (
+                    <div>
+                      <div key={p.petId} className="list-item">
+                        <div
+                          className="list-title"
+                          onClick={() => {
+                            nav(`/petinfo/detail?detailID=${p.petId}`);
+                          }}
+                        >
+                          <ImgWrapper
+                            src={process.env.REACT_APP_API_URL + p.petImage}
+                            alt={"내 펫 이미지"}
+                            width="30px"
+                            height="30px"
+                            borderRadius="50%"
+                            defaultImg={defaultImg}
+                          />
+                        </div>
+                        {/* 펫 이름이 이미지 우측에 위치하도록 스타일링 */}
+                        <div>{p.petName}</div>
+                        <div style={{ display: "flex", width: "100%" }}>
+                          <div className="list-reg-user"></div>
+                          <div className="list-reg-dt">
+                            등록일: {ctime.toISOString()}
+                          </div>
+                          <div>
+                            <button
+                              className="btn_delete"
+                              onClick={() => {
+                                if (!window.confirm("정말 삭제하시겠습니까?")) {
+                                } else {
+                                  deleteApi({
+                                    url: `/api/pet/${p.petId}`,
+                                  }).then((resp) => {
+                                    console.log(resp);
+                                    if (resp.status === 200) {
+                                      // nav(-1);
+                                    }
+                                  });
+                                }
+                              }}
+                            >
+                              삭제
+                            </button>
+                          </div>                          
+                        </div>
+                        
+                      </div>
                     </div>
-                  </div>
-                </div>
-              })}
+                  );
+                })}
             </div>
           </div>
+          <BtnFloat
+            onClick={() => {
+              nav("/petinfo/insert");
+            }}
+          />
         </div>
-        
-        <BtnFloat
-          onClick={() => {
-            nav("/petinfo/insert");
-          }}
-        />
         <BottomTabNavigation />
       </div>
-    
+    </LayoutUserExist>
   );
 }
-
 export default PetList;
